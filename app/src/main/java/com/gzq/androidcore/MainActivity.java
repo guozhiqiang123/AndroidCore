253c036eb45883b2546f8fff101ac452576f33fb
GZQ <774550196@qq.com>
2018-12-29 18:03:09 +0800

    update

--- app/src/main/java/com/gzq/androidcore/MainActivity.java
+++ app/src/main/java/com/gzq/androidcore/MainActivity.java
@@ -9,6 +9,9 @@ import com.gzq.lib_bluetooth.BluetoothSearchHelper;
 import com.gzq.lib_bluetooth.ConnectListener;
 import com.gzq.lib_bluetooth.IBluetoothView;
 import com.gzq.lib_bluetooth.SearchListener;
+import com.gzq.lib_pay.OnSuccessAndErrorListener;
+import com.gzq.lib_pay.alipay.AliPayUtils;
+import com.gzq.lib_pay.wechat.WechatPayUtils;
 import com.gzq.lib_resource.mvp.base.BaseActivity;
 import com.gzq.lib_resource.mvp.base.BasePresenter;
 import com.gzq.lib_resource.mvp.base.IPresenter;
@@ -23,49 +26,29 @@ public class MainActivity extends BaseActivity implements IBluetoothView{
     @Override
     public void initParams(Intent intentArgument, Bundle bundleArgument) {
 
-        BluetoothSearchHelper bluetoothSearchHelper = new BluetoothSearchHelper();
-        //每次8秒，搜索3次
-        bluetoothSearchHelper.searchBle(8000, 3, new SearchListener() {
+        AliPayUtils.aliPay(this, "后台获取的订单信息", new OnSuccessAndErrorListener() {
             @Override
-            public void onSearching(boolean isOn) {
-                //isOn=true:正在搜索，isOn=false:停止搜索
+            public void onSuccess(String s) {
+
             }
 
             @Override
-            public void onNewDeviceFinded(BluetoothDevice newDevice) {
-                //搜索到新设备
-            }
+            public void onError(String s) {
 
-            @Override
-            public void obtainDevice(BluetoothDevice device) {
-                //搜索到目标设备
-            }
-
-            @Override
-            public void noneFind() {
-                //没有搜索到目标设备
-            }
-        },"Ble1","Ble2");
-
-        bluetoothSearchHelper.clear();
-
-        BluetoothConnectHelper bluetoothConnectHelper=new BluetoothConnectHelper();
-        bluetoothConnectHelper.connect("1B:F4:E2:8A:85:AB", new ConnectListener() {
-            @Override
-            public void success(BluetoothDevice device) {
-                //连接成功
-            }
-
-            @Override
-            public void failed() {
-                //连接失败
-            }
-
-            @Override
-            public void disConnect(String address) {
-                //断开连接
             }
         });
+        WechatPayUtils.wechatPayApp(this, "AppId", "商户号", "私钥",
+                "预支付交易会话ID", "发起支付的时间戳", "签名", new OnSuccessAndErrorListener() {
+                    @Override
+                    public void onSuccess(String s) {
+
+                    }
+
+                    @Override
+                    public void onError(String s) {
+
+                    }
+                });
     }
 
     @Override
